5976b186e9bb8d7329962013cb271fd237a2cbab
yoorhim <choi.yoorhim@gmail.com>
2021-01-10 13:50:09 -0500

    Inititial commit

---
+++ lesson_2/pseudocode_practice.rb
@@ -0,0 +1,12 @@
+#problem 1: a method that returns the sum of two integers
+
+#START
+
+#SET method that sums two integers
+def number_sum(number1, number2)
+  number1 + number2
+end
+
+#PRINT method
+puts number_sum(1,2)
+#END
