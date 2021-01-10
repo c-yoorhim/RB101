91a52a20fc0a19d192d1481f019bc61d5eb5eac4
yoorhim <choi.yoorhim@gmail.com>
2021-01-09 22:26:11 -0500

    Initial commit

---
+++ lesson_2/calculator.rb
@@ -0,0 +1,36 @@
+#asks for two numbers; convert to integer
+
+Kernel.puts("Provide first number.")
+first_num = Kernel.gets().chomp().to_i()
+Kernel.puts("Provide second number.")
+second_num = Kernel.gets().chomp().to_i()
+
+loop do 
+  
+  #asks for the type of operation to perform: add, subtract, multiply or divide
+  Kernel.puts("Provide type of operation to perform: add, subtract, multiply or divide.")
+  operation = Kernel.gets().chomp()
+
+  #displays the result
+  case operation
+  when "add"
+    Kernel.puts(first_num + second_num)
+    break
+  when "subtract"
+    Kernel.puts(first_num - second_num)
+    break
+  when "multiply"
+    Kernel.puts(first_num*second_num)
+    break
+  when "divide"
+    if second_num == 0
+      Kernel.puts("You cannot divide by 0.")
+      break
+    else
+      Kernel.puts(first_num/second_num)
+      break
+    end
+  else Kernel.puts("Please provide a proper type of operation.")
+  end
+
+end
