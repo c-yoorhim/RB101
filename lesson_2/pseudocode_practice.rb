2195c1db4d881fbcb01fb354c14850d242096643
yoorhim <choi.yoorhim@gmail.com>
2021-01-10 14:08:35 -0500

    Added psuedocode problem #2

--- lesson_2/pseudocode_practice.rb
+++ lesson_2/pseudocode_practice.rb
@@ -8,5 +8,21 @@ def number_sum(number1, number2)
 end
 
 #PRINT method
-puts number_sum(1,2)
+#puts number_sum(1,2)
 #END
+
+#problem 2: a method that takes an array of strings, 
+#and returns a string that is all those strings concatenated together
+
+#START
+#SET new array of strings
+words = %w[hi my name is yoorhim]
+#WHILE iterator <= array length
+  #PRINT string value
+words.each {|word| print(word + " ")}
+#END
+
+#problem 3: a method that takes an array of integers, 
+#and returns a new array with every other element
+
+#SET new array of integers
