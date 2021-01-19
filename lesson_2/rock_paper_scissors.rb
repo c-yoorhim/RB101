3237e5afff78d76fe08718a4b9670734358917e4
yoorhim <choi.yoorhim@gmail.com>
2021-01-18 21:54:53 -0500

    Updated rock_paper_scissors.rb for bonus features

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,9 +1,11 @@
-VALID_CHOICES = %w(rock paper scissors)
+VALID_CHOICES = {r: "rock", p: "paper", s: "scissors", l: "lizard", sp: "spock"}
 
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+  (first == 'rock' && (second == 'scissors' || second == 'lizard')) ||
+    (first == 'paper' && (second == 'rock' || second == 'spock')) ||
+    (first == 'scissors' && (second == 'paper'|| second == 'lizard')) ||
+    (first == 'lizard' && (second == 'spock' || second == 'paper')) ||
+    (first == 'spock' && (second == 'scissor' || second == 'rock'))
 end
 
 def display_results(player, computer)
@@ -15,8 +17,9 @@ def display_results(player, computer)
   end
 end
 
-# ask user to make a choice
+total_score = {player:0, computer: 0}
 
+# ask user to make a choice
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
@@ -24,27 +27,38 @@ end
 loop do
   choice = ''
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = Kernel.gets().chomp()
+    loop do
+    
+      prompt("Choose one: ")
+      VALID_CHOICES.each {|k,v| puts k.to_s + " for " + v + "; "}
+      choice = Kernel.gets().chomp().to_sym
 
-    # validate that user made the right choice
+      if VALID_CHOICES.key?(choice)
+        break
+      else
+        prompt("That's not a valid choice.")
+      end
+    end
 
-    if VALID_CHOICES.include?(choice)
+    computer_choice = VALID_CHOICES.values.sample
+  
+    prompt("You chose: #{VALID_CHOICES[choice]}. Computer chose: #{computer_choice}.")
+    display_results(VALID_CHOICES[choice], computer_choice)
+    
+    #lookup until conditional?
+    if total_score.value?(5)
+      prompt("Grand winner is: #{total_score.key(5).to_s}!")
       break
     else
-      prompt("That's not a valid choice.")
+      if win?(VALID_CHOICES[choice], computer_choice)
+        total_score[:player] += 1
+      else win?(computer_choice, VALID_CHOICES[choice])
+        total_score[:computer] += 1
+      end
+      prompt("Current score:")
+      total_score.each {|k,v| puts k.to_s + " " + v.to_s + "; "}
     end
   end
-
-  # here we notice that there's a lot of overlap:
-  # array choices with computer below same as user above.
-  # indicates opportunity to create constant. See VALID_CHOHICES above.
-
-  computer_choice = VALID_CHOICES.sample
-
-  prompt("You chose: #{choice}. Computer chose: #{computer_choice}.")
-
-  display_results(choice, computer_choice)
 
   prompt("Do you want to play agian?")
   answer = Kernel.gets().chomp()
