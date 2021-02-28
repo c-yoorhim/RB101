1fc079afe1c692de25e9b1b865978d3f8f21c3cf
yoorhim <choi.yoorhim@gmail.com>
2021-02-27 21:14:46 -0500

    More easy_4.rb problems.

--- small_problems/easy_4.rb
+++ small_problems/easy_4.rb
@@ -108,26 +108,48 @@ when year % 4 == leap
 
 =end
 
+def leap_year?(year)
+  year % 400 == 0 ? true : ( 
+    year % 100 == 0 ? false : ( 
+      year % 4 == 0 ? true : false
+    )
+  )
+end
+
+# # explore further
 # def leap_year?(year)
-#   year % 400 == 0 ? true : ( 
-#     year % 100 == 0 ? false : ( 
-#       year % 4 == 0 ? true : false
-#     )
-#   )
+#   if year % 4 == 0
+#     if year % 100 == 0
+#       if year % 400 == 0
+#         true
+#       else false
+#       end
+#     else true
+#     end 
+#   else false
+#   end 
 # end
 
-# explore further
+=begin
+Problem 4: Leap Years (Part 2)
+Input: 
+Output:
+Explicit:
+- The British Empire adopted the Gregorian Calendar in 1752, which was a leap year.
+- Prior to 1752, the Julian Calendar was used.
+- Under the Julian Calendar, leap years occur in any year that is evenly divisible by 4.
+- Using this information, update the method from the previous exercise to determine leap years both before and after 1752.
+=end
+
 def leap_year?(year)
-  if year % 4 == 0
-    if year % 100 == 0
-      if year % 400 == 0
-        true
-      else false
-      end
-    else true
-    end 
-  else false
-  end 
+  if year > 1752
+    year % 400 == 0 ? true : ( 
+      year % 100 == 0 ? false : ( 
+        year % 4 == 0 ? true : false
+      )
+    )
+  else year % 4 == 0
+  end
 end
 
 # p leap_year?(2016) == true
@@ -139,14 +161,108 @@ end
 # p leap_year?(2000) == true
 # p leap_year?(1900) == false
 # p leap_year?(1752) == true
-# p leap_year?(1700) == false
+# p leap_year?(1700) == true
 # p leap_year?(1) == false
-# p leap_year?(100) == false
+# p leap_year?(100) == true
 # p leap_year?(400) == true
 
 =begin
-Problem 4: 
-Input:
-Output:
+Problem 5: Multiples of 3 and 5
+Input: integer 
+Output: integer
 Explicit:
+- Write a method that searches for all multiples of 3 or 5 that lie between 1 and some other number
+- then computes the sum of those multiples. 
+- For instance, if the supplied number is 20, the result should be 98 (3 + 5 + 6 + 9 + 10 + 12 + 15 + 18 + 20).
+- You may assume that the number passed in is an integer greater than 1.
+
+Data:
+- integer
+- array
+
+Algorithm:
+- For (1..int): select numbers when int % 3== 0 or int % 5 == 0
+- add sum of all the elements in the array
 =end
+
+def multisum(int)
+  (1..int).select { |num| (num % 3 == 0) || (num % 5 == 0) }.reduce(:+)
+end
+
+# p multisum(3) == 3
+# p multisum(5) == 8
+# p multisum(10) == 33
+# p multisum(1000) == 234168
+
+=begin
+Problem 6: Running Totals
+Input: array (of int)
+Output: array of same size
+Explicit:
+- Write a method that takes an Array of numbers
+- returns an Array with the same number of elements, and each element has the running total from the original Array.
+- [] returns []
+
+Algorithm:
+1. iterate through the argument array
+2. create a variable to store sum and returned as the new elemtn in that position
+
+=end
+
+def running_total(array)
+  sum = 0
+  array.map { |num| sum += num }
+
+  # sum = 0
+  # array.each_with_object([]) do |num, new_array|
+  #   sum += num
+  #   new_array << sum
+  # end
+
+  # array.inject([]) { |new_arr, num| new_arr << new_arr.last.to_i + num }
+end
+
+# p running_total([2, 5, 13]) == [2, 7, 20]
+# p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
+# p running_total([3]) == [3]
+# p running_total([]) == []
+
+=begin
+Problem 7: Convert a String to a Number!
+Input:  str (of digits)
+Output: return num as an integer
+Explicit:
+- The String#to_i method converts a string of numeric characters (including an optional plus or minus sign) to an Integer.
+- String#to_i and the Integer constructor (Integer()) behave similarly. 
+- In this exercise, you will create a method that does the same thing.
+- Write a method that takes a String of digits, 
+- and returns the appropriate number as an integer
+- You may not use any of the methods mentioned above.
+- For now, do not worry about leading + or - signs, nor should you worry about invalid characters; 
+- assume all characters will be numeric.
+- You may not use any of the standard conversion methods available in Ruby to convert a string to a number
+   - such as String#to_i, Integer(), etc. Your method should do this the old-fashioned way and calculate the result by analyzing the characters in the string.
+
+Algorithm:
+1. NUMBERS = {"0" => 0, "1" => 1, "2" => 2, "3" => 3...}
+2. iterate through string, and check which corresponding integer value it is from the NUMBERS hash
+3. put each digit in an array
+4. find size of array
+5. iterate through array and sum: num * 10^array array, reduce array size by 1
+6. return sum
+
+=end
+
+NUMBERS = {'0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9 }
+
+def string_to_integer(str_int)
+  exp = str_int.length - 1
+  int_array = str_int.chars.each_with_object([]) do |str, arr| 
+                arr << NUMBERS[str] * (10**exp)
+                exp -= 1
+              end
+  int_array.inject(:+)
+end
+
+p string_to_integer('4321') == 4321
+p string_to_integer('570') == 570
