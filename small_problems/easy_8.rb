ab637574dfe00ea08b29fe83e4997bd8cb47f286
yoorhim <choi.yoorhim@gmail.com>
2021-03-14 13:59:18 -0400

    All Easy 8 problems.

--- small_problems/easy_8.rb
+++ small_problems/easy_8.rb
@@ -140,13 +140,160 @@ def palindromes(str)
                  .select { |substring| substring == substring.reverse }
 end
 
-p palindromes('abcd') == []
-p palindromes('madam') == ['madam', 'ada']
-p palindromes('hello-madam-did-madam-goodbye') == [
-  'll', '-madam-', '-madam-did-madam-', 'madam', 'madam-did-madam', 'ada',
-  'adam-did-mada', 'dam-did-mad', 'am-did-ma', 'm-did-m', '-did-', 'did',
-  '-madam-', 'madam', 'ada', 'oo'
-]
-p palindromes('knitting cassettes') == [
-  'nittin', 'itti', 'tt', 'ss', 'settes', 'ette', 'tt'
-]
+# p palindromes('abcd') == []
+# p palindromes('madam') == ['madam', 'ada']
+# p palindromes('hello-madam-did-madam-goodbye') == [
+#   'll', '-madam-', '-madam-did-madam-', 'madam', 'madam-did-madam', 'ada',
+#   'adam-did-mada', 'dam-did-mad', 'am-did-ma', 'm-did-m', '-did-', 'did',
+#   '-madam-', 'madam', 'ada', 'oo'
+# ]
+# p palindromes('knitting cassettes') == [
+#   'nittin', 'itti', 'tt', 'ss', 'settes', 'ette', 'tt'
+# ]
+
+=begin
+Problem 6: fizzbuzz 
+Input: int, int
+Output: 
+Explicit:
+- Write a method that takes two arguments: the first is the starting number, and the second is the ending number.
+- Print out all numbers between the two numbers,
+  - except if: a number is divisible by 3, print "Fizz",
+  - if a number is divisible by 5, print "Buzz",
+  - if a number is divisible by 3 and 5, print "FizzBuzz".
+
+Algorithm:
+- iterate through range from (start_int..end_int)
+- if num passed % 3 == 0 && % 5 == 0, print "FizzBuzz"
+- if num passed % 3 == 0, print "Fizz"
+- elsif num passed % 5 == 0, print "Buzz"
+- else print num
+=end
+
+def fizzbuzz(start_int, end_int)
+  (start_int..end_int).each_with_object("") do |num, string|
+    if (num % 3 == 0) && (num % 5 == 0) 
+      string << "Fizzbuzz"
+    elsif num % 3 == 0
+      string << "Fizz"
+    elsif num % 5 == 0
+      string << "Buzz"
+    else string << num.to_s
+    end
+    string << ", " unless num == end_int
+  end
+end
+
+# p fizzbuzz(1, 15) # -> 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
+
+=begin
+Problem 7: Double Char (Part 1) 
+Input: str
+Output: new str
+Explicit:
+- Write a method that takes a string
+- returns a new string in which every character is doubled.
+Algorithm:
+- iterate through the string's char
+- new_str << char * 2
+=end
+def repeater(str)
+  str.chars.each_with_object("") do |char, new_str|
+    new_str << char*2
+  end
+end
+
+# p repeater('Hello') == "HHeelllloo"
+# p repeater("Good job!") == "GGoooodd  jjoobb!!"
+# p repeater('') == ''
+
+=begin
+Problem 8: Double Char (Part 2) 
+Input: str
+Output: new str
+Explicit:
+- Write a method that takes a string
+- returns a new string in which every consonant character is doubled.
+- Vowels (a,e,i,o,u), digits, punctuation, and whitespace should not be doubled.
+
+Algorithm: regex
+- create a conditional with regex
+  - how do i account for digits, punctuation and whitespace?
+=end
+
+CONSONANTS = (('a'..'z').to_a + ('A'..'Z').to_a).select { |char| char =~ /[^aeiou]/i }
+
+def double_consonants(str)
+  str.chars.each_with_object("") do |char, new_str|
+    if CONSONANTS.include?(char)
+      new_str << char*2
+    else new_str << char
+    end
+  end
+end
+
+# p double_consonants('String') == "SSttrrinngg"
+# p double_consonants("Hello-World!") == "HHellllo-WWorrlldd!"
+# p double_consonants("July 4th") == "JJullyy 4tthh"
+# p double_consonants('') == ""
+
+=begin
+Problem 9: Reverse the Digits In a Number
+Input: pos int
+Output: int 
+Explicit:
+- Write a method that takes a positive integer as an argument
+- returns that number with its digits reversed.
+Implicit:
+- no leading zeros
+
+Algorithm:
+- convert int to str
+- str.reverse
+- convert back to int
+
+=end
+
+def reversed_number(int)
+  int.to_s.reverse.to_i
+end
+
+# p reversed_number(12345) == 54321
+# p reversed_number(12213) == 31221
+# p reversed_number(456) == 654
+# p reversed_number(12000) == 21 # No leading zeros in return value!
+# p reversed_number(12003) == 30021
+# p reversed_number(1) == 1
+
+=begin
+Problem 10: Get The Middle Character
+Input: non empty str
+Output: str
+Explicit:
+- Write a method that takes a non-empty string argument
+- and returns the middle character or characters of the argument.
+- If the argument has an odd length, you should return exactly one character.
+- If the argument has an even length, you should return exactly two characters.
+Implicit:
+- spaces count as char
+- single char return itself
+
+Algorithm:
+1 is length even? is it odd?
+2 Find the center
+- for odd, return 1 char at the center
+- for even, return 2 char at the center
+- return char
+=end
+
+def center_of(str)
+  middle = (str.length / 2)
+  return str[middle] if str.length.odd?
+  return str[(middle-1)..(middle)]
+end
+
+p center_of('I love ruby') == 'e'
+p center_of('Launch School') == ' '
+p center_of('Launch') == 'un'
+p center_of('Launchschool') == 'hs'
+p center_of('x') == 'x'
