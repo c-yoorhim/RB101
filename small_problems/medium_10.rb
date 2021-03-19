6d7bda3a5f1dfdb383453a5352d13c5f90633733
yoorhim <choi.yoorhim@gmail.com>
2021-03-18 23:37:13 -0400

    More medium 10 problems

--- small_problems/medium_10.rb
+++ small_problems/medium_10.rb
@@ -169,3 +169,125 @@ end
 # p on_lights(5) == [1,4]
 # p on_lights(10) == [1,4,9]
 # p on_lights(1000) == [1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, 361, 400, 441, 484, 529, 576, 625, 676, 729, 784, 841, 900, 961]
+
+=begin
+Problem 5: Diamonds!
+- Write a method that displays a 4-pointed diamond in an n x n grid, 
+  where n is an odd integer that is supplied as an argument to the method.
+  You may assume that the argument will always be an odd integer.
+  
+Input: odd integer
+Output: 4 pointed diamond in a nxn grid; string
+
+Explicit:
+
+Example:
+diamond(1) =>
+
+*
+diamond(3)
+
+ *         1: n/2 spaces, n*
+***        2: row == n/2   
+ *
+
+ diamond(9)
+
+    *         1: n/2 spaces, n* 
+   ***         
+  *****   3 - 2 spaces, 5 *
+ *******  4 - 1 space, 7 * 
+********* 5
+ *******  6
+  *****   7 - 4
+   ***    8 - 6
+    *     9 - 7
+
+1 = return *
+ELSE find mid row: 3=> 2; 9 => 5; n/2 + 1
+(1..n) iterate throug |row|
+  row: (midrow - 1)*spaces +  "*" (n-(spaces_count*2))* *
+  row+1:(midrow - 2)*spaces + "*" *  UNTIL row == midrow
+  after that:
+  minus_row = 2
+  row = row - (minus_row)
+  minus_row -= 2
+  row+1= row - (minus_row)
+  UNTIL minus_row < 1
+
+=end
+
+def diamond(n)
+  dia = '*'
+  return dia if n == 1
+  row_print_array = first_half_diamond(n).reverse
+  row_print_array.shift
+  row_print_array.each { |str| puts str }
+  ''
+end
+
+def first_half_diamond(n)
+  dia = '*'
+  space = ' '
+  mid_row= n/2 + 1
+  space_count = mid_row - 1
+  (1..mid_row).to_a.each_with_object([]) do |row, row_print_array|
+    row_print = (space * space_count) + (dia * (n - (space_count*2)))
+    puts row_print
+    row_print_array << row_print.dup
+    space_count -= 1
+  end
+end
+# #center, #upto, #downto
+# p diamond(9)
+
+=begin
+Problem 6: Stack Machine Interpretation
+
+A stack is a list of values that grows and shrinks dynamically. 
+In ruby, a stack is easily implemented as an Array that just uses the #push and #pop methods.
+A stack-and-register programming language is a language that uses a stack of values. 
+Each operation in the language operates on a register, which can be thought of as the current value. 
+The register is not part of the stack. 
+Operations that require two values pop the topmost item from the stack:
+  that is, the operation removes the most recently pushed value from the stack, 
+  perform the operation using the popped value and the register value,
+  and then store the result back in the register.
+Consider a MULT operation in a stack-and-register language. 
+It multiplies the stack value with the register value, 
+  removes the value from the stack, 
+  and then stores the result back in the register. 
+Thus, if we start with a stack of 3 6 4 (where 4 is the topmost item in the stack), 
+  and a register value of 7, 
+  then the MULT operation will transform things to 3 6 on the stack (the 4 is removed), 
+  and the result of the multiplication, 28, is left in the register. 
+  If we do another MULT at this point, 
+    then the stack is transformed to 3, and the register is left with the value 168.
+
+ [3,6,4]
+ register = 7
+ 
+ MULT =
+[3,6,4].pop * 7 = 28
+MULT =
+[3,6].pop * 28 = 168
+
+Input: str
+Output: int
+Explicit: 
+- Write a method that implements a miniature stack-and-register-based programming language that has the following commands:
+  - n: Place a value n in the "register". Do not modify the stack.
+  - PUSH: Push the register value on to the stack. Leave the value in the register.
+  - ADD: Pops a value from the stack and adds it to the register value, storing the result in the register.
+  - SUB: Pops a value from the stack and subtracts it from the register value, storing the result in the register.
+  - MULT: Pops a value from the stack and multiplies it by the register value, storing the result in the register.
+  - DIV: Pops a value from the stack and divides it into the register value, storing the integer result in the register.
+  - MOD: Pops a value from the stack and divides it into the register value, storing the integer remainder of the division in the register.
+  - POP: Remove the topmost item from the stack and place in register
+  - PRINT: Print the register value
+All operations are integer operations (which is only important with DIV and MOD).
+Programs will be supplied to your language method via a string passed in as an argument. 
+Your program may assume that all programs are correct programs; 
+  that is, they won't do anything like try to pop a non-existent value from the stack, and they won't contain unknown tokens.
+You should initialize the register to 0.
+
